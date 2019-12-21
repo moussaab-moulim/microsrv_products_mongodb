0d4a007ff89a0699b921a05b5b1f64f86ba051a2
moussaab moulim <moussaabmma@gmail.com>
2019-12-21 04:03:57 +0000

    init project

---
+++ src/test/java/com/moussaabmma/microsrv/MicrosercvicesTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moussaabmma.microsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosercvicesTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
