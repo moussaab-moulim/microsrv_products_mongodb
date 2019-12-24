c224e2800b31ff9ba25930115f1af95ada304128
moussaab moulim <moussaabmma@gmail.com>
2019-12-24 10:40:12 +0000

    authorize access to products and categories on get method by all visitors

--- src/main/java/com/moussaabmma/microsrv/security/SecurityConfig.java
+++ src/main/java/com/moussaabmma/microsrv/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.moussaabmma.microsrv.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +20,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"categories/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"products/**").permitAll();
         http.authorizeRequests().antMatchers("categories/**").hasAuthority("ADMIN");
         http.authorizeRequests().antMatchers("products/**").hasAuthority("USER");
         http.authorizeRequests().anyRequest().authenticated();
